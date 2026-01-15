e43c5146aa16f089dfec6fc9d5834e5e56c8dc5b
ayush <ayush.agrawal@vedak.com>
2026-01-15 19:20:06 +0530

    formatting

--- migrations/20260115134736-seeding-category-values.js
+++ migrations/20260115134736-seeding-category-values.js
@@ -1,34 +1,34 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('categories', [
-      {
-        name: 'Adventure',
-        slug: 'adventure',
-      },
-      {
-        name: 'Cultural',
-        slug: 'cultural',
-      },
-      {
-        name: 'Women-Only',
-        slug: 'women-only',
-      },
-      {
-        name: 'Weekend',
-        slug: 'weekend',
-      },
-      {
-        name: 'International',
-        slug: 'international',
-      },
-    ]);
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('categories', [
+            {
+                name: 'Adventure',
+                slug: 'adventure',
+            },
+            {
+                name: 'Cultural',
+                slug: 'cultural',
+            },
+            {
+                name: 'Women-Only',
+                slug: 'women-only',
+            },
+            {
+                name: 'Weekend',
+                slug: 'weekend',
+            },
+            {
+                name: 'International',
+                slug: 'international',
+            },
+        ]);
+    },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('categories', {
-      slug: ['adventure', 'cultural', 'women-only', 'weekend', 'international'],
-    });
-  },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('categories', {
+            slug: ['adventure', 'cultural', 'women-only', 'weekend', 'international'],
+        });
+    },
 };
